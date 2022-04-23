d15c802f86b5f1a34daf169e72ba1418fa4c29f4
Tigercat <tigercat0828@gmail.com>
2022-04-23 23:23:33 +0800

    update

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,31 +1,16 @@
 ﻿using Leetcode;
+using Leetcode.Common;
 using Leetcode.Problems;
 using System.Collections;
 using static System.Console;
 
 
 
-ListNode A = new(1);
-ListNode B = new(2);
-ListNode C = new(3);
-
-
-A.next = B;
-B.next = C;
-;
-
-
-P206_Reverse_Linked_List problem = new();
-problem.ReverseList2(A);
-
-
-
-public class ListNode {
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null) {
-        this.val = val;
-        this.next = next;
-    }
-
+Random random = new Random();
+for (int i = 0; i < 10; i++) {
+    Console.WriteLine(30);
+    Console.WriteLine(random.Next(1, (int)MathF.Pow(2, 29)));
 }
+
+P779_Kth_Symbol_in_Grammar P = new();
+P.KthGrammar(30, 515676024);
