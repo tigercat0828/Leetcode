fa01c1acf652201cef9ee6eb4fc3b0d77ed86739
Tigercat <tigercat0828@gmail.com>
2022-05-02 11:57:30 +0800

    new leetcode problem
    - 242
    - 36
    - 383
    - 387
    - 74

---
+++ Leetcode/Problems/P242_Valid_Anagram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems {
+    public class P242_Valid_Anagram {
+
+        // O(n)
+        public bool IsAnagram(string s, string t) {
+            int[] dictS = new int[26];
+            int[] dictT = new int[26];
+            foreach (char ch in s) {
+                dictS[ch - 'a']++;
+            }
+            foreach (char ch in t) {
+                dictT[ch - 'a']++;
+            }
+            for (int i = 0; i < dictS.Length; i++) {
+                int sc = dictS[s[i] - 'a'];
+                int tc = dictT[t[i] - 'a'];
+                if (sc != tc) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Leetcode/Problems/P383_Ransom_Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems {
+    public class P383_Ransom_Note {
+        public bool CanConstruct(string ransomNote, string magazine) {
+            int i = 0;
+            int j = 0;
+            for (int i = 0; i < ; i++) {
+
+            }
+            return false;
+        }
+    }
+}
