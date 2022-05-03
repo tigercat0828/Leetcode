12f106c7d6769a6aa5fbe83b681ce0db1be1a177
Tigercat <tigercat0828@gmail.com>
2022-05-03 22:43:25 +0800

    new problems
    374, 383, 69, 704

--- Leetcode/Problems/P383_Ransom_Note.cs
+++ Leetcode/Problems/P383_Ransom_Note.cs
@@ -6,13 +6,24 @@ using System.Threading.Tasks;
 
 namespace Leetcode.Problems {
     public class P383_Ransom_Note {
+        // O(n)
         public bool CanConstruct(string ransomNote, string magazine) {
-            int i = 0;
-            int j = 0;
-            for (int i = 0; i < ; i++) {
-
+            int[] dictRasom = new int[26];
+            int[] dictMagzine = new int[26];
+            for (int i = 0; i < ransomNote.Length; i++) {
+                int key = ransomNote[i] - 'a';
+                dictRasom[key]++;
             }
-            return false;
+            for (int i = 0; i < magazine.Length; i++) {
+                int key = magazine[i] - 'a';
+                dictMagzine[key]++;
+            }
+            for (int i = 0; i < 26; i++) {
+                if (dictRasom[i] > dictMagzine[i]) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
---
+++ Leetcode/Problems/P704_Binary_Search.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems {
+    public class P704_Binary_Search {
+        public int Search(int[] nums, int target) {
+            return search(nums, target, 0, nums.Length - 1);
+        }
+        private int search(int[] nums, int target, int left, int right) {
+            if (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] < target) {
+                    return search(nums, target, mid + 1, right);
+                }
+                else {
+                    return search(nums, target, left, mid - 1);
+                }
+            }
+            return -1;
+        }
+        public int Search2(int[] nums, int target) {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                    continue;
+                }
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                    continue;
+                }
+            }
+            return -1;
+        }
+    }
+}
