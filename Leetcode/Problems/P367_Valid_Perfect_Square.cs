1d3394e2208f18879ce2a133f70cb006201c624d
Tigercat <tigercat0828@gmail.com>
2022-05-05 23:19:46 +0800

    new problem
    1385, 232, 367

---
+++ Leetcode/Problems/P367_Valid_Perfect_Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems {
+    public class P367_Valid_Perfect_Square {
+        // binary search O(logn)
+        public bool IsPerfectSquare(int num) {
+            int left = 1;
+            int right = num;
+            int result = search(num, left, right);
+            return result >= 1;
+
+        }
+        private int search(int target, int left, int right) {
+
+            if (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if ((float)mid == (float)target / mid) {
+                    return mid;
+                }
+                if (mid > target / mid) {
+                    return search(target, left, mid - 1);
+                }
+                else {
+                    return search(target, mid + 1, right);
+                }
+            }
+            return 0;
+        }
+        // using built-in library, bad
+        public bool IsPerfectSquare2(int num) {
+            int sqrt = (int)Math.Sqrt(num);
+            return num == sqrt * sqrt;
+        }
+    }
+}
