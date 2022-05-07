3fb1fff75cd1173e3fd9cc312490d858a0a946a7
Tigercat <tigercat0828@gmail.com>
2022-05-07 16:11:13 +0800

    new problems
    278, 744

---
+++ Leetcode/Problems/P278_First_Bad_Version.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems {
+    public class P278_First_Bad_Version {
+        public int FirstBadVersion(int n) {
+            if (n <= 0) {
+                return -2; // Input error;
+            }
+            int left = 1;
+            int right = n;
+            // minify the check range by half each iterate
+            while (left <= right) {
+
+                int mid = left + (right - left) / 2;
+                if (IsBadVersion(mid)) {
+                    // check older version
+                    right = mid - 1;
+                }
+                else {
+                    // check newer version
+                    left = mid + 1;
+                }
+            }
+            if (IsBadVersion(left)) return left;
+            return -1; // No bad version
+        }
+        int bad;
+        private bool IsBadVersion(int n) {
+            return bad == n;
+        }
+    }
+}
---
+++ Leetcode/Problems/P744_Find_Smallest_Letter_Greater_Than_Target.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems {
+    public class P744_Find_Smallest_Letter_Greater_Than_Target {
+        public class Solution {
+            public char NextGreatestLetter(char[] letters, char target) {
+                for (int i = 0; i < letters.Length; i++) {
+
+                    if ((int)target < (int)letters[i]) {
+                        return letters[i];
+                    }
+                }
+                return letters[0];
+            }
+        }
+    }
+}
