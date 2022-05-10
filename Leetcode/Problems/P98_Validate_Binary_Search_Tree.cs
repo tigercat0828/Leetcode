937b440f008911f3e98039b4b9ecd8e5307987ca
Tigercat <tigercat0828@gmail.com>
2022-05-10 23:26:25 +0800

    new problem
    235, 653, 98

---
+++ Leetcode/Problems/P98_Validate_Binary_Search_Tree.cs
@@ -0,0 +1,16 @@
+﻿using Leetcode.Common;
+
+namespace Leetcode.Problems {
+    public  class P98_Validate_Binary_Search_Tree {
+        public bool IsValidBST(TreeNode root) {
+            return check(root, long.MinValue, long.MaxValue);   
+        }
+        private bool check(TreeNode root, long min, long max) {
+            if (root == null) return true;
+            if(root.val <= min || root.val >= max) return false;
+            bool left = check(root.left, min, root.val);
+            bool right = check(root.right, root.val, max);
+            return left && right;
+        }
+    }
+}
