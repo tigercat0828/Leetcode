9a5e7fa56e732e467eec627fcb6731b774825b01
Tigercat <tigercat0828@gmail.com>
2022-05-19 22:00:17 +0800

    new problems
    136, 231, 542

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,29 +1,6 @@
-﻿Solution s = new Solution();
+﻿string problemString = Console.ReadLine();
+string[] tokens = problemString.Split(' ');
+tokens[0] = tokens[0].Remove(tokens[0].Length-1);
 
-int[] nums = { 1, 3, 5, 6 };
-
-Console.WriteLine(s.SearchInsert(nums, 2));
-Console.WriteLine(s.SearchInsert(nums, 7));
-
-
-
-public class Solution {
-    public int SearchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.Length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-
-        }
-        return left;
-    }
-}
+string className = "P" + string.Join('_', tokens);
+Console.WriteLine(className);
