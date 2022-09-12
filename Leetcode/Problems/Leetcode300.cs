75dc1eca737418d52f47bd50e1a725da4c3ed707
Tigercat <tigercat0828@gmail.com>
2022-09-12 17:18:11 +0800

    update

---
+++ Leetcode/Problems/Leetcode300.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems {
+    public  class Leetcode300 {
+        // DP bottom-up
+        /*
+         * State Transition Equation
+         * L[i] 代表至該節點的遞增長度
+         * L[i] = max{L[j], j | 0<j<i and A[j]<A[i]}
+         */
+
+        // faster (no "new list" cost)
+        public int LengthOfLIS(int[] nums) {
+            int[] Len = new int[nums.Length];
+            
+            for (int i = 0; i < nums.Length; i++) {
+
+                int count = 0;
+                // 0<j<i and A[j]<A[i]
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] < nums[i]) {
+                        count = Math.Max(count, Len[j]);
+                    }
+                }
+                Len[i] = count+1;
+            }
+            return Len.Max();
+        }
+        public int LengthOfLIS2(int[] nums) {
+            int[] Len = new int[nums.Length];
+            Array.Fill(Len, 1);
+            for (int i = 1; i < nums.Length; i++) {
+
+                List<int> subproblem = new();
+                // 0<j<i and A[j]<A[i]
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] < nums[i]) {
+                        subproblem.Add(Len[j]);
+                    }
+                }
+                if (subproblem.Count > 0) { 
+                    Len[i] = subproblem.Max() + 1;
+                }
+            }
+            return Len.Max();
+        }
+    }
+}
--- Leetcode/Problems/Leetcode322.cs
+++ Leetcode/Problems/Leetcode322.cs
@@ -1,7 +1,33 @@
 ﻿namespace Leetcode.Problems {
-
-    public class P322_Coin_Change {
+    public class Leetcode322 {
+        // dp approach bottom-up
         public int CoinChange(int[] coins, int amount) {
+
+            /* state transition function
+             * R[i] 代表硬幣數
+             * R[i] = min(R[i], R[i-coins[i]]+1);
+             */
+            int[] remain = new int[amount + 1];
+            Array.Fill(remain, -1);
+            remain[0] = 0;
+            for (int i = 1; i <= amount; i++) {
+                foreach (var coin in coins) {
+                    if (i - coin < 0) continue;             
+                    if(remain[i - coin] == -1) continue;    // i-coin 也沒有答案,換下一個coin測試
+
+                    if (remain[i] == -1) {
+                        remain[i] = remain[i - coin] + 1;
+                    }
+                    else {
+                        remain[i] = Math.Min(remain[i], remain[i - coin] + 1);
+                    }
+                }
+            }
+            return remain[amount];
+        }
+
+        // BFS approach
+        public int CoinChange2(int[] coins, int amount) {
             Queue<Node> queue = new Queue<Node>();
             bool[] dict = new bool[10001];
             queue.Enqueue(new Node(amount, 0));
