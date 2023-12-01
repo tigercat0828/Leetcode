49c80c37c2244b3a46808993935989258b0f17d6
Tigercat <tigercat0828@gmail.com>
2023-12-01 21:54:12 +0800

    new problem
    leetcode 1662
    leetcode 1845

---
+++ Leetcode.CSharp/Problems/Leetcode1662.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CSharp.Problems {
+    public  class Leetcode1662 {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
+            string str1 = string.Join("",word1);
+            string str2 = string.Join("", word2);
+            return str1 == str2;
+        }
+    }
+}
---
+++ Leetcode.CSharp/Problems/Leetcode1845.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CSharp.Problems; 
+public class Leetcode1845 {
+    public class SeatManager {
+        int customerNum;
+        public SeatManager(int n) {
+            customerNum = 0;
+        }
+
+        public int Reserve() {
+            customerNum++;
+            return customerNum;
+        }
+
+        public void Unreserve(int seatNumber) {
+            customerNum--;
+        }
+    }
+
+}
