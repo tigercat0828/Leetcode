7ea7261c879a12be408be7460842b84c74122d63
Tigercat <tigercat0828@gmail.com>
2024-09-05 14:34:47 +0800

    new solution

---
+++ Leetcode.CSharp/Solutions/Leetcode2022.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CSharp.Solutions {
+    public class Leetcode2022 {
+        public int[][] Construct2DArray(int[] original, int m, int n) {
+
+            if (m*n>original.Length) return [];
+
+            int[][] matrix = new int[m][];
+            for (int i = 0; i < m; i++) {
+                matrix[i] = new int[n];
+            }
+            for (int i = 0; i < original.Length; i++) {
+                int row = i/m;
+                int col = i%n;
+                matrix[row][col] = original[i];
+            }
+            return matrix;
+        }
+    }
+}
